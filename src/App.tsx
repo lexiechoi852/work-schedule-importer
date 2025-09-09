88d6567712f614fb7bcada393c9b636d15013e94
Lexie Choi <lexiechoi852@gmail.com>
2025-09-09 00:21:53 -0400

    chore: updated eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+
+import type { JSX } from "react";
+
+const App = (): JSX.Element => {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <React.Fragment>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((c) => c + 1)} type="button">
           count is {count}
         </button>
         <p>
@@ -28,8 +21,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root element not found");
+}
+
+createRoot(container).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
